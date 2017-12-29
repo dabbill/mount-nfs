9f8b6d2bebe4bd11a360a0fbe8bc49e257279d54
Ryan Gillette <rgillette@2ndwatch.com>
2017-12-29 10:07:22 -0800

    Changed imports to be shorter

--- mount-nfs.py
+++ mount-nfs.py
@@ -1,16 +1,16 @@
 #! /usr/bin/env python3
 
-from os.path import ismount
-from sys import argv
-from subprocess import call
+import os.path
+import sys
+import subprocess
 
 def server_status(nfs_host):
     with open('/dev/null', 'a') as out:
-        response = call(['ping', '-c1', nfs_host], stdout=out, stderr=out)
+        response = subprocess.call(['ping', '-c1', nfs_host], stdout=out, stderr=out)
         return not response
 
 
-user_input1 = str(argv[1])
+user_input1 = str(sys.argv[1])
 
 host = '192.168.1.10'
 mnt = '/mnt/NFS/'
@@ -19,18 +19,18 @@ unmount = 'sudo umount'
 
 ping_status = server_status(host)
 
-is_mounted = ismount('/mnt/NFS/plex')
+is_mounted = os.path.ismount('/mnt/NFS/plex')
 
 if user_input1 == 'mount':
     if is_mounted:
         print('Already mounted silly Master!')
     else:
         if ping_status:
-            call(['sudo', 'mount', '-t', 'nfs', '192.168.1.10:/mnt/tank/data', '/mnt/NFS/data'])
-            call(['sudo', 'mount', '-t', 'nfs', '192.168.1.10:/mnt/tank/backup', '/mnt/NFS/backup'])
-            call(['sudo', 'mount', '-t', 'nfs', '192.168.1.10:/mnt/tank/plex', '/mnt/NFS/plex'])
-            call(['sudo', 'mount', '-t', 'nfs', '192.168.1.10:/mnt/Torrents', '/mnt/NFS/torrents'])
-            call(['sudo', 'mount', '-t', 'nfs', '192.168.1.10:/mnt/tank/Zene', '/mnt/NFS/Zene'])
+            subprocess.call(['sudo', 'mount', '-t', 'nfs', '192.168.1.10:/mnt/tank/data', '/mnt/NFS/data'])
+            subprocess.call(['sudo', 'mount', '-t', 'nfs', '192.168.1.10:/mnt/tank/backup', '/mnt/NFS/backup'])
+            subprocess.call(['sudo', 'mount', '-t', 'nfs', '192.168.1.10:/mnt/tank/plex', '/mnt/NFS/plex'])
+            subprocess.call(['sudo', 'mount', '-t', 'nfs', '192.168.1.10:/mnt/Torrents', '/mnt/NFS/torrents'])
+            subprocess.call(['sudo', 'mount', '-t', 'nfs', '192.168.1.10:/mnt/tank/Zene', '/mnt/NFS/Zene'])
             print('NFS Mounts have been connected. Master!')
         else:
             print('No connection to NFS server. Master!')
@@ -38,19 +38,19 @@ if user_input1 == 'mount':
 if user_input1 == 'unmount':
     if is_mounted:
         if ping_status:
-            call(['sudo', 'umount', '/mnt/NFS/plex'])
-            call(['sudo', 'umount', '/mnt/NFS/data'])
-            call(['sudo', 'umount', '/mnt/NFS/backup'])
-            call(['sudo', 'umount', '/mnt/NFS/torrents'])
-            call(['sudo', 'umount', '/mnt/NFS/Zene'])
+            subprocess.call(['sudo', 'umount', '/mnt/NFS/plex'])
+            subprocess.call(['sudo', 'umount', '/mnt/NFS/data'])
+            subprocess.call(['sudo', 'umount', '/mnt/NFS/backup'])
+            subprocess.call(['sudo', 'umount', '/mnt/NFS/torrents'])
+            subprocess.call(['sudo', 'umount', '/mnt/NFS/Zene'])
             print('NFS Mounts have been disconnected!')
 
         else:
-            call(['sudo', 'umount', '-l', '/mnt/NFS/plex'])
-            call(['sudo', 'umount', '-l', '/mnt/NFS/data'])
-            call(['sudo', 'umount', '-l', '/mnt/NFS/backup'])
-            call(['sudo', 'umount', '-l', '/mnt/NFS/torrents'])
-            call(['sudo', 'umount', '-l', '/mnt/NFS/Zene'])
+            subprocess.call(['sudo', 'umount', '-l', '/mnt/NFS/plex'])
+            subprocess.call(['sudo', 'umount', '-l', '/mnt/NFS/data'])
+            subprocess.call(['sudo', 'umount', '-l', '/mnt/NFS/backup'])
+            subprocess.call(['sudo', 'umount', '-l', '/mnt/NFS/torrents'])
+            subprocess.call(['sudo', 'umount', '-l', '/mnt/NFS/Zene'])
             print('NFS Mounts have been forcefully disconnected!')
     else:
         print('NFS Mounts are already disconnected. Master!')
